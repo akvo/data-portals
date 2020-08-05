f0504d51fc071ec1a230e331f2084ec4cf9daa3c
zuhdil <zuhdil@gmail.com>
2020-08-05 18:07:39 +0700

    Push to Google Cloud

--- ci/deploy-semaphoreci.sh
+++ ci/deploy-semaphoreci.sh
@@ -27,31 +27,25 @@ gcloud config set container/use_client_certificate False
 if [[ "${CI_TAG:-}" =~ promote-.* ]]; then
     log Environment is production
     gcloud container clusters get-credentials production
-    K8S_CONFIG_FILE=ci/k8s/config-prod.yml
 else
     log Environement is test
     gcloud container clusters get-credentials test
-    K8S_CONFIG_FILE=ci/k8s/config-test.yml
-
-    log Creating statsd to prometheus
-    docker build -t "eu.gcr.io/${PROJECT_NAME}/rsr-statsd-to-prometheus:${CI_COMMIT}" statsd-to-prometheus
 
     log Pushing images
     gcloud auth configure-docker
-    docker push eu.gcr.io/${PROJECT_NAME}/rsr-backend
-    docker push eu.gcr.io/${PROJECT_NAME}/rsr-nginx
-    docker push eu.gcr.io/${PROJECT_NAME}/rsr-statsd-to-prometheus
-
+    docker push eu.gcr.io/${PROJECT_NAME}/data-portals-backend
+    docker push eu.gcr.io/${PROJECT_NAME}/data-portals-frontend
+    docker push eu.gcr.io/${PROJECT_NAME}/data-portals-nginx
 fi
 
-sed -e "s/\${TRAVIS_COMMIT}/$CI_COMMIT/" ci/k8s/deployment.yml > deployment.yml.tmp
-
-kubectl apply -f ${K8S_CONFIG_FILE}
-kubectl apply -f ci/k8s/memcached.yml
-kubectl apply -f ci/k8s/media-disk.yml
-kubectl apply -f ci/k8s/service.yml
-kubectl apply -f deployment.yml.tmp
-kubectl apply -f ci/k8s/grafana/main.yml
-
-log Waiting for k8s to finish
-./ci/k8s/helpers/wait-for-k8s-deployment-to-be-ready.sh
+# sed -e "s/\${TRAVIS_COMMIT}/$CI_COMMIT/" ci/k8s/deployment.yml > deployment.yml.tmp
+# 
+# kubectl apply -f ${K8S_CONFIG_FILE}
+# kubectl apply -f ci/k8s/memcached.yml
+# kubectl apply -f ci/k8s/media-disk.yml
+# kubectl apply -f ci/k8s/service.yml
+# kubectl apply -f deployment.yml.tmp
+# kubectl apply -f ci/k8s/grafana/main.yml
+# 
+# log Waiting for k8s to finish
+# ./ci/k8s/helpers/wait-for-k8s-deployment-to-be-ready.sh
